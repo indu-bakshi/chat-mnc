024d53d652a56beabcb8daec9ec67e5769c14bd9
Indu Bakshi <bakshi.webworld@gmail.com>
2021-05-18 20:59:36 +0530

    chat app

---
+++ src/components/Chat/chat.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+
+import "./Chat.css";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
+import TextContainer from "../TextContainer/TextContainer";
+
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "https://react-chat-app-mnc.herokuapp.com/";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    // console.log(socket);
+    socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      socket.emit("disconnect");
+
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+    return () => {
+      socket.off();
+    };
+  }, [messages]);
+
+  useEffect(() => {
+    // socket.on("message", (message) => {
+    //   setMessages([...messages, message]);
+    // });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+  console.log(message, messages);
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+        <br />
+
+        {/* <input value={message} 
+               onChange={(event)=> setMessage(event.target.value)} onKeyPress={event=> event.key === 'Enter' ? sendMessage(event): null}
+               /> */}
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
+};
+
+export default Chat;
